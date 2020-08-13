fabb815c1d06d28c46c83a441986bc7be803b956
daikikawahata <daikikawahata@icloud.com>
2020-08-13 14:52:03 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,10 @@ class PostsController < ApplicationController
       post.update(checked: false)
     else
       post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 
-  item =Pos.find(params[:id])
-  render json: { post: item }
 end
